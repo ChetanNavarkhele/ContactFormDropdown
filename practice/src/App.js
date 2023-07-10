8307b2bb0b85f1195b7849deffd9ca692aef4d5e
Chetan Navarkhele <chetannavarkhele@gmail.com>
2023-07-10 11:45:42 +0530

    done

---
+++ practice/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Task2A from './task-2A/Main';
+import Task2B from './task-2B/Main';
+import Task3 from './task-3/Main';
+import Task4 from './task-4/Main';
+
+
+function App() {
+  return (
+    <div>
+      <Task2A/>
+      <Task2B/>
+      <Task3/>
+      <Task4/>
+    </div>
+  );
+}
+
+export default App;
